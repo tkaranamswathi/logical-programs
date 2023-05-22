822373366f28d4bcbb7c2953ae3d79572c5b6a2d
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-22 12:54:39 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace UC1_FIBONACCI
+{
+    internal class Program
+    {
+       public static void Main(string[] args)
+       { 
+            Console.WriteLine("welcome to fibonacci");
+            Console.WriteLine("please enter any number");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int a = 0, b = 1, c = 0;
+            Console.Write("{0} {1}", a, b);
+            for (int i = 2; i < n; i++)
+            {
+                c = a + b;
+                Console.Write(" {0}", c);
+                a = b;
+                b = c;
+            }
+             
+       }
+    }
+}
