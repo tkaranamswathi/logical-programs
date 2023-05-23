bd865f354028f8e506ed8fa1290e5de8a1b97234
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-23 15:33:54 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿namespace UC4REVERSEPROGRAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to reverse number");
+            Console.Write("Enter a Number : ");
+            int number = int.Parse(Console.ReadLine());
+            int reminder, reverse = 0;
+            while (number > 0)
+            {
+                reminder = number % 10;
+                reverse = (reverse * 10) + reminder;
+                number = number / 10;
+            }
+            Console.WriteLine($"The Reverse order is : {reverse}");
+            Console.ReadKey();
+        }
+    }
+}
