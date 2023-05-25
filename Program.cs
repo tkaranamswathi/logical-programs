5a97cd983290470ef129799f77fcdf088da6d586
tkaranamswathi <[swathitk1999@gmail.com]git config --global user.name [tkaranamswathi]>
2023-05-25 09:44:21 +0530

    [swathi] adding welcome message

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters;
+using System.Security.Cryptography;
+
+namespace UC5COUPONNUMBERS
+{
+    class program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to coupon  numbers");
+            Random random = new Random();
+            String str = "abcdefghijklmnopqrstuvwxyz0123456789";
+            int size = 6;
+            int n= 0;
+            Console.WriteLine("please enter any number for how many number of times to generate a coupon ");
+            int x=Convert.ToInt32(Console.ReadLine());
+          for (n = 0; n < x; n++) 
+            {
+                Console.WriteLine(n);
+                String randomstring = " ";
+
+                for (int i = 0; i < size; i++)
+                {
+                    int y = random.Next(str.Length);
+                    randomstring = randomstring + str[y];
+                }
+                Console.WriteLine("coupon code:" + randomstring);
+            }
+        }
+    }
+}
