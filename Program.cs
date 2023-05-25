9a3c1c36663be958b8a14d67b460f301ef894e52
tkaranamswathi <134041544+tkaranamswathi@users.noreply.github.com>
2023-05-25 12:16:07 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@
             Console.WriteLine("welcome to fibonacci");
             Console.WriteLine("please enter any number");
             int n=Convert.ToInt32(Console.ReadLine());
-            int a = 0, b = 1, c = 0;
+            int a = 0, b = 1, sum = 0;
             Console.Write("{0} {1}", a, b);
             for (int i = 2; i < n; i++)
             {
-                c = a + b;
+                sum = a + b;
                 Console.Write(" {0}", c);
                 a = b;
-                b = c;
+                b = sum;
             }
              
        }
