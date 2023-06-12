0350e3ec67e6e10c1b16d19477c15092eb10ce4b
tkaranamswathi <134041544+tkaranamswathi@users.noreply.github.com>
2023-06-12 11:54:57 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,6 +2,14 @@
 {
     internal class Program
     {
-     
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine("HI");
+            }
+            stopwatch.Stop();
+            Console.WriteLine("Time Elapsed : {0}",
+            stopwatch.Elapsed);
     }
 }
